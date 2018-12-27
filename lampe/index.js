6e3cff24fc35d69d818e2c926567fe15956df998
Niklas Kinze <niklas.kinze@gmail.com>
2018-12-27 15:28:11 +0100

    Begrüßung gefixed

--- lampe/index.js
+++ lampe/index.js
@@ -1,30 +1,36 @@
 module.change_code = 1;
 'use strict';
-
+//Skill als Skill festlegen
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'lampe' );
-
-
-app.launch( function( req, res ) {
-	response.say( 'Sag einfach stelle meine Lampe auf' ).shouldEndSession( false );
+//App id von "lampe"
+app.id = "amzn1.ask.skill.8bbdfce1-1a21-4d0c-a817-251916573f4a";
+//Begrüßungsnachricht
+app.launch( function(req, res) {
+	res.say('Wie kann ich helfen?');
 } );
 
-
-app.error = function( exception, request, response ) {
+//Falls ein Error passieren sollte, sehr unwahrscheinlich
+app.error = function(exception, req, res) {
 	console.log(exception)
 	console.log(request);
 	console.log(response);
-	response.say( 'Sorry da ist was schief gelaufen ' + error.message);
+	res.say( 'Sorry da ist was schief gelaufen ' + error.message);
 };
-
-app.intent('An',
+//intent lamp, bis jetzt der einzige
+app.intent('lamp',
   {
-    "slots":{"FARBE":"AMAZON.Color"}
-	,"utterances":[
-		"Stell meine Lampe auf {FARBE}",
-		"Lampe auf {FARBE}",
-		"Lass meine Lampe in {FARBE} leuchten"]
+	//Einziger slot, FARBE -> Einzige Variable die benötigt wird
+  "slots":
+			{"FARBE":"AMAZON.Color"},
+	//Sätze die benutzt werden können
+	"utterances":[
+			"Stell meine Lampe auf {FARBE}",
+			"Lampe auf {FARBE}",
+			"{FARBE}",
+			"Lass meine Lampe in {FARBE} leuchten"]
   },
+	//Antwort von Alexa, FARBE wird ausgegeben
   function(req,res) {
     var farbe = req.slot('FARBE');
     res.say("Deine Farbe Lautet: "+farbe);
